886677a7de757796768baa0c2885ac4bff9b13c6
Huy Hua <huanamhuy@gmail.com>
2023-07-31 17:53:49 +0700

    Initial Commit

---
+++ MovieApp/MovieAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieAppApp.swift
+//  MovieApp
+//
+//  Created by Huy Hua Nam on 31/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct MovieAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
