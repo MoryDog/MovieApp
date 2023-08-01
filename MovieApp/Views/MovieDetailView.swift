b6fa323f73daa54116b504bf92fdc0d86db27a91
Huy Hua <huanamhuy@gmail.com>
2023-08-01 18:44:35 +0700

    add LoadingView and MovieDetailView, empty

---
+++ MovieApp/Views/MovieDetailView.swift
@@ -0,0 +1,41 @@
+//
+//  MovieDetail.swift
+//  MovieApp
+//
+//  Created by Huy Hua Nam on 01/08/2023.
+//
+
+import SwiftUI
+
+struct MovieDetailView: View {
+    let movieId: Int
+    @ObservedObject private var movieDetailState = MovieDetailState()
+    
+    var body: some View {
+        ZStack {}
+    }
+}
+
+struct MovieDetailImage: View {
+    @ObservedObject var imageLoader: ImageLoader
+    let imageURL: URL
+    
+    var body: some View {
+        ZStack{
+            Rectangle().fill(Color.gray.opacity(0.3))
+            if self.imageLoader.image != nil {
+                Image(uiImage: self.imageLoader.image!).resizable()
+            }
+        }
+        .aspectRatio(16/9, contentMode: .fit)
+        .onAppear {
+            self.imageLoader.loadImage(with: self.imageURL)
+        }
+    }
+}
+
+struct MovieDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        MovieDetailView(movieId: Movie.sampleMovie.id)
+    }
+}
